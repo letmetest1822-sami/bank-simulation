d762efc8c63dc1648c93ac39160bc6da06f908ba
letmetest1822-sami <letmetest1822@gmail.com>
2023-09-09 13:14:13 -0400

    AccountRepository and Impl created

--- src/main/java/com/cydeo/service/impl/AccountServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/AccountServiceImpl.java
@@ -2,14 +2,24 @@ package com.cydeo.service.impl;
 
 import com.cydeo.enums.AccountType;
 import com.cydeo.model.Account;
+import com.cydeo.repository.AccountRepository;
 import com.cydeo.service.AccountService;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Component
 public class AccountServiceImpl implements AccountService {
+
+    AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
     @Override
     public Account createNewAccount(BigDecimal balance, Date createDate, AccountType accountType, Long userId) {
         //we need to create Account Object
@@ -30,7 +40,7 @@ public class AccountServiceImpl implements AccountService {
         //return the object created
 
 
-        return null;
+        return accountRepository.save(account);
     }
 
     @Override
